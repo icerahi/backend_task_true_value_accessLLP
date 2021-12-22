a0a78b28797b4fff47cda331071d36a432035c5d
icerahi <zanjarwhite@gmail.com>
2021-12-22 13:30:04 +0600

    swagger documentation added

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
      'rest_framework',
      'import_export',
      'django_filters',
+     'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+             'libraries' : {
+                'staticfiles': 'django.templatetags.static', 
+            }
         },
     },
 ]
@@ -122,7 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILSEDIRS=[
+    BASE_DIR/'static-root'
+]
+STATIC_ROOT=BASE_DIR/'static-root'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -137,7 +144,7 @@ REST_FRAMEWORK = {
     'ORDERING_PARAM':'sort',
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-    )
-
-    
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+   
 }
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.schemas import get_schema_view
+from django.conf.urls.static import static
+from django.conf import settings
+...
+from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
+
+
+schema_view = get_schema_view(title='Users API', renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
+
 
 urlpatterns = [
+    path('',schema_view),
     path('admin/', admin.site.urls),
     path('api/',include('app.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
